c21f45f724ad50d652968c97f5759b8f1394d525
turtle-nest <n78693107@gmail.com>
2025-01-14 18:27:13 +0100

    Fix task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):
-	if chr(i) not in ['e', 'q']:
-		print(chr(i), end="")
+print("".join("{:c}".format(i) for i in range(97, 123) if i not in [101, 113]), end="")
