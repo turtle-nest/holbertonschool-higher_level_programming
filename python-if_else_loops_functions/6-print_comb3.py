4ca453ac25fe0dac2955fd541608b252fbf41c44
turtle-nest <n78693107@gmail.com>
2025-01-16 11:12:13 +0100

    Fix pycodestyle

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print(", ".join("{:d}{:d}".format(i, j) for i in range(10) for j in range(i + 1, 10)))
+print(", ".join(
+    "{:d}{:d}".format(i, j) for i in range(10) for j in range(i + 1, 10)
+))
