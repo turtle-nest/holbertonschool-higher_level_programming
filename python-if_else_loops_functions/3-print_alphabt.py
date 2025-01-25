02ec5dfb9ba3c16b4d123699e9cd1715cd39a5cd
turtle-nest <n78693107@gmail.com>
2025-01-25 16:29:19 +0100

    New implementation

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-print(
-    "".join("{:c}".format(i) for i in range(97, 123) if i not in [101, 113]),
-    end=""
-)
+for i in range (97, 123):
+    if i not in [101, 113]:
+        print(chr(i), end="")
+print()
