06f85cfce458864add207f165ba09ee2deaedf0d
turtle-nest <n78693107@gmail.com>
2025-01-27 15:38:03 +0100

    Add task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""This module defines a square with a private attribut size"""
+
 class Square:
-    """Defines a Square with a private attribute size"""
-    def __init__(self, __size):
-        self.size = __size
+    """Represents a square"""
+
+    def __init__(self, size):
+        """
+        Initializes the square with a private attribut size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
