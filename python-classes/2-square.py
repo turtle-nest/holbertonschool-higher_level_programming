dd05d2d0619235dcd7d4c43fe024d8b8b27f11c9
turtle-nest <n78693107@gmail.com>
2025-01-27 16:02:07 +0100

    Add task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""This module describes a square by a private instance attribute size."""
+
 class Square:
     """Defines a square"""
+
     def __init__(self, size=0):
-        self.size = size
+        """
+        Initializes the square with a private instance attribute size.
+
+        Args:
+            size (int): The private instance attribute.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
