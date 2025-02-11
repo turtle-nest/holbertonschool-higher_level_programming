954f6631006b45e3e2ef461bf3510f07b839af92
turtle-nest <n78693107@gmail.com>
2025-02-11 09:13:08 +0100

    Fix task 3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+
+"""This is an example of the to_json_string function
+>>> to_json_string = __import__('5-to_json_string').to_json_string
+>>> my_list = [1, 2, 3]
+>>> s_my_list = to_json_string(my_list)
+>>> print(s_my_list)
+>>> print(type(s_my_list))
+[1, 2, 3]
+<class 'str'>
+"""
+
 from json import dumps
 
 def to_json_string(my_obj):
