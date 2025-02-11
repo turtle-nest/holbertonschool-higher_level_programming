4f875a941a9ef06f9e9775e6e20d4f678c4199a9
turtle-nest <n78693107@gmail.com>
2025-02-11 11:14:07 +0100

    Add task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module that represent a student"""
+
+class Student:
+    """A class student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance. 
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance.
+
+        If `attrs` is a list of strings, only those attributes are
+        retrieved. Otherwise, all attributes are retrieved.
+
+        Args:
+            attrs (list, optional): List of attribute names to include.
+                                    Defaults to None.
+
+        Returns:
+            dict: Dictionary representation of the instance.
+        """
+        if isinstance(attrs, list):
+            if all(isinstance(attr, str) for attr in attrs):
+                return {key: getattr(self, key) for key in attrs
+                    if hasattr(self, key)}
+        return self.__dict__
