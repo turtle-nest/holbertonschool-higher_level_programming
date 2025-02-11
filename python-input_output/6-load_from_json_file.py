0ef55665d613e66e796a8ae59703868a000b8f21
turtle-nest <n78693107@gmail.com>
2025-02-11 09:00:53 +0100

    Add task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a JSON file.
+    
+    Args:
+    filename: The name of the file containing the JSON string.
+    
+    Returns:
+    The Python object represented by the JSON string.
+    """
+    with open(filename, 'r') as file:
+        return json.load(file)
