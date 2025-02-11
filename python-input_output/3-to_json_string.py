0753f6cd2f6aef5d54be6fe70b962872c3de2b01
turtle-nest <n78693107@gmail.com>
2025-02-11 08:54:57 +0100

    Correct docstrings

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,5 +2,13 @@
 import json
 
 def to_json_string(my_obj):
-    """Returns the JSON representation of an object."""
+    """
+    Returns the JSON representation of an object (string).
+    
+    Args:
+    my_obj: The object to be serialized to JSON.
+    
+    Returns:
+    A string containing the JSON representation of the object.
+    """
     return json.dumps(my_obj)
