9f13455c3e17cb2dade7cab9249bbb4a53953910
turtle-nest <n78693107@gmail.com>
2025-02-18 18:26:23 +0100

    Add a correction of task 2

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,50 @@
+#!/bin/usr/python3
+"""This module uses the requests library,
+    parse and manipulate JSON data using Python,
+    convert structured data into CSV format.
+"""
+
+import requests
+import csv
+
+def fetch_and_print_posts():
+    """Fetch posts from JSONPlaceholder and print the titles."""
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url) # Send the GET request
+
+    print(f"Statuts Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json() # Conversion to JSON
+        for post in posts:
+            print(post["title"])
+    else:
+        print("Failed to retrivie posts")
+
+def fetch_and_save_posts():
+    """Fetch posts from JSONPlaceholder and save them into a CSV file."""
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        # Data structure as a list of dictionaries
+        posts_data = [{"id": post["id"],
+                       "title": post["title"],
+                       "body": post["body"]}
+                       for post in posts]
+
+        with open("posts.csv",
+                  mode="w",
+                  newline="",
+                  encoding="utf-8") as file:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+            writer.writeheader() #
+            writer.writerows(posts_data)
+
+        print("Data successfully saved to posts.csv")
+    else:
+        print("Failed to retrieve posts")
