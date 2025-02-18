c098d3002285828dc6e6feae6077496dc3aa349e
turtle-nest <n78693107@gmail.com>
2025-02-18 19:27:11 +0100

    Add a correction of task 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -40,20 +40,20 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
         elif self.path == '/data':
             self.send_response(200)
             self.send_header("Content-type", "application/json")
-            self.end_header()
+            self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode('utf-8'))
 
         elif self.path == '/status':
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
-            self.end_header()
+            self.end_headers()
             self.wfile.write(b"OK")
 
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
-            self.end_header()
+            self.end_headers()
             self.wfile.write(b"Endpoint not found")
 
 
