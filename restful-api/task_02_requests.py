ce492dc1e3b73bbd281ef3c414afb0ad73a78995
turtle-nest <n78693107@gmail.com>
2025-02-20 06:58:53 +0100

    Correction of task 2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -19,6 +19,8 @@ def fetch_and_print_posts():
         posts = response.json()
         for post in posts:
             print(post["title"])
+    else:
+        print("Failed to retrivie posts")
 
 
 def fetch_and_save_posts():
@@ -29,16 +31,20 @@ def fetch_and_save_posts():
     if response.status_code == 200:
         posts = response.json()
 
-        fieldnames = ["id", "title", "body"]
+        posts_data = [
+            {"id": post["id"], "title": post["title"], "body": post["body"]}
+            for post in posts
+        ]
 
         with open("posts.csv",
                   mode="w",
                   newline="",
                   encoding="utf-8") as file:
+            fieldnames = ["id", "title", "body"]
             writer = csv.DictWriter(file, fieldnames=fieldnames)
 
             writer.writeheader()
-            writer.writerows(posts)
+            writer.writerows(posts_data)
 
         print("Data successfully saved to posts.csv")
     else:
