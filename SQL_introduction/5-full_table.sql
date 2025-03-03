a40e68fa097cc822187d7af991c455e25bed1a39
turtle-nest <n78693107@gmail.com>
2025-03-03 13:04:19 +0100

    Fixed

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,4 @@
 -- Full description
 -- a script that prints the following description of the table first_table from the database hbtn_0c_0
-
--- Use the database hbtn_0c_0
-USE hbtn_0c_0;
-
 -- Get the full description of the table first_table
 SHOW CREATE TABLE first_table;
