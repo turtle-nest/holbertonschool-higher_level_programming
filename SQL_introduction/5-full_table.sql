b5afcbd94fba7c78f9ed0591b3498339e81c363e
turtle-nest <n78693107@gmail.com>
2025-03-03 13:38:35 +0100

    List all in table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,3 @@
--- Full description
--- a script that prints the following description of the table first_table from the database hbtn_0c_0
+-- A script that prints the following description of the table first_table from the database hbtn_0c_0
 -- Get the full description of the table first_table
 SHOW CREATE TABLE first_table;
---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- List all in table
+-- List all rows of the table first_table
+SELECT * FROM first_table;
