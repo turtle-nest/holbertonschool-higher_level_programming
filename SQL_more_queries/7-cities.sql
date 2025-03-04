c14d3ccab74a67dcd304b10edf3aa4ee45c8a559
turtle-nest <n78693107@gmail.com>
2025-03-04 19:09:44 +0100

    7. Cities table

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,16 @@
+-- Cities table
+-- a script that creates the database hbtn_0d_usa and the table cities
+
+-- Create the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use database hbtn_0d_usa
+USE hbtn_0d_usa;
+
+-- Create the table cities
+CREATE TABLE IF NOT EXISTS cities (
+	id INT AUTO_INCREMENT NOT NULL UNIQUE,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
