5e312353cff7dbf5f0a83b3789e88ba83edd00c6
turtle-nest <n78693107@gmail.com>
2025-03-04 09:12:24 +0100

    9. Full creation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,16 @@
+-- Full creation
+-- a script that creates a table called second_table in the database hbtn_0c_0 and adds multiple rows
+
+-- Create the table second_table if it does not exist
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+-- Insert multiple rows into the table second_table
+INSERT INTO second_table (id, name, score)
+VALUES
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'Georges', 8);
