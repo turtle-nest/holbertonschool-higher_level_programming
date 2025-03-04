6f36599173dfd8cd5f1cb43da07c30427c1d0567
turtle-nest <n78693107@gmail.com>
2025-03-04 18:46:31 +0100

    4. ID can't be null

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,8 @@
+-- ID can't be null
+-- a script that creates the table id_not_null
+
+-- Create table id_not_null
+CREATE TABLE IF NOT EXISTS id_not_null (
+	id INT NOT NULL DEFAULT 1,
+	name VARCHAR(256)
+);
