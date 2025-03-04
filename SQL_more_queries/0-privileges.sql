4d0cfbd73d7f9143986bf5fd50850e45f649f45c
turtle-nest <n78693107@gmail.com>
2025-03-04 11:07:38 +0100

    0. My privileges!

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,8 @@
+-- My privileges!
+-- a script that lists all privileges of the MySQL users
+
+-- List all privileges of the user user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List all privileges of the user user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
