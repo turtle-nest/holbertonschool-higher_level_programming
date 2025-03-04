89d22341937b8c6f32ead5bcd9e253f8c5eb0992
turtle-nest <n78693107@gmail.com>
2025-03-04 18:22:28 +0100

    Fixed all privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,9 @@
 -- Root user
 -- a script that creates the MySQL server user user_0d_1
+
+-- Create the user user_0d_1 if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
