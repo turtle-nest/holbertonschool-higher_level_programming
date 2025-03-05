72c5706d607c13e8a7d88eeccb812c8dd6c1e137
turtle-nest <n78693107@gmail.com>
2025-03-05 09:26:16 +0100

    Fixed with using database

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,9 @@
 -- Cities by States
 -- a script that lists all cities contained in the database
 
+-- Use database hbtn_0d_usa
+USE hbtn_0d_usa;
+
 -- List all cities by states
 SELECT cities.id, cities.name, states.name AS state_name
 FROM cities
