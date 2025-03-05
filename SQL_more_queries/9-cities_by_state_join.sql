3872c978d1ef6c92f7c5280469fd8231b78d67cf
turtle-nest <n78693107@gmail.com>
2025-03-05 09:28:37 +0100

    Remove AS

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -5,7 +5,8 @@
 USE hbtn_0d_usa;
 
 -- List all cities by states
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = states.id
+JOIN states
+ON cities.state_id = states.id
 ORDER BY cities.id ASC;
