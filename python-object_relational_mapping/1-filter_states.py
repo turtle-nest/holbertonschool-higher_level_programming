2a01e88c485b810cf05b3bed96b05f129f9312c5
turtle-nest <n78693107@gmail.com>
2025-03-06 17:19:03 +0100

    Fixed indentation after passwd=password (removed a space)

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
 
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=db_name)
+                        passwd=password, db=db_name)
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' \
