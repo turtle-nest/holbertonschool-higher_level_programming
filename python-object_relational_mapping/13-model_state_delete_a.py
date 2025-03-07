d2307c768852d644931cf732c789d385e67fc502
turtle-nest <n78693107@gmail.com>
2025-03-07 01:43:05 +0100

    Delete with a

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A script that changes the name of a State object from the database.
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    deleted_state = session.query(State).filter(State.name.like('%a%'))\
+                           .order_by(State.id).all()
+    for state in deleted_state:
+        session.delete(state)
+
+    session.commit()
+    session.close()
