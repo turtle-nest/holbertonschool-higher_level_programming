dfbbfb09e74681bd14d96285f465a42da3cb4ee0
turtle-nest <n78693107@gmail.com>
2025-03-07 13:41:47 +0100

    14. Try to fix pycodestyle

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -21,8 +21,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    cities = session.query(City, State).join(
-        State, City.state_id == State.id).order_by(City.id).all()
+    cities = session.query(City, State).join(State, City.state_id == State.id).order_by(City.id).all()
 
     for city, state in cities:
         print(f"{state.name}: ({city.id}) {city.name}")
