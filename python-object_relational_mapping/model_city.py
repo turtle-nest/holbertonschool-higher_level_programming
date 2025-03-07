f12fc0015f5a7242b6225ec859efd2fe9da6af2f
turtle-nest <n78693107@gmail.com>
2025-03-07 13:33:02 +0100

    Add docstrings in model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,9 +1,15 @@
+#!/usr/bin/python3
+"""
+Module that contains class definition of a City
+and an instance Base = declarative_base()
+"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class City(Base):
+    """City class"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
