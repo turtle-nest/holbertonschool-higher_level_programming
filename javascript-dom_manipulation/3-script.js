0773baf16db2039119a700f678200fbe466da4df
turtle-nest <n78693107@gmail.com>
2025-04-02 12:14:21 +0200

    Add 2 and 3 (It works !)

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,9 @@
+document.querySelector('#toggle_header').addEventListener('click', () => {
+  const header = document.querySelector('header');
+
+  if (header.classList.contains('red')) {
+    header.classList.replace('red', 'green');
+  } else {
+    header.classList.replace('green', 'red');
+  }
+});
